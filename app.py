d8a8a051995040eb2fe9155f7d4d970b9ef82a61
Saurabh Parikh <saurabh.parikh@missionbio.com>
2021-02-25 00:48:32 -0800

    Add dropping functionality

--- app.py
+++ app.py
@@ -18,15 +18,17 @@ interface.subheader('GUI for Mosaic built using Streamlit')
 interface.status('v0.1.3')
 
 sample, should_save, save_name = load.run()
+save.run(sample, save_name, should_save)
 
 current_assay, available_assays = preprocess.run(sample)
 
 prepare.run(current_assay, available_assays)
 cluster.run(current_assay, available_assays)
-customize.run(current_assay)
-save.run(sample, save_name, should_save)
 
-visual.run(sample, current_assay)
+sample_kept, current_assay_kept = customize.run(sample, current_assay)
 
+visual_type = visual.run(sample_kept, current_assay_kept)
+
+current_assay.add_metadata(DFT.VISUAL_TYPE, visual_type)
 for a in available_assays:
     a.add_metadata(DFT.INITIALIZE, False)
