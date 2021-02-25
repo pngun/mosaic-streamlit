851fe1711e4c05ec05d85a973f0e36d65cb4a270
Saurabh Parikh <saurabh.parikh@missionbio.com>
2021-02-25 00:56:18 -0800

    Save filtered h5

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
 import interface
-import defaults as DFT
 from tasks import (
     load,
     preprocess,
@@ -18,7 +17,6 @@ interface.subheader('GUI for Mosaic built using Streamlit')
 interface.status('v0.1.3')
 
 sample, should_save, save_name = load.run()
-save.run(sample, save_name, should_save)
 
 current_assay, available_assays = preprocess.run(sample)
 
@@ -29,6 +27,7 @@ sample_kept, current_assay_kept = customize.run(sample, current_assay)
 
 visual_type = visual.run(sample_kept, current_assay_kept)
 
-current_assay.add_metadata(DFT.VISUAL_TYPE, visual_type)
-for a in available_assays:
-    a.add_metadata(DFT.INITIALIZE, False)
+if should_save:
+    save.run(sample_kept, save_name)
+
+save.store_metadata(sample, current_assay, visual_type, available_assays)
