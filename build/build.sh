4c0fd9f75845cb473737a3798311168be796f662
Aljosa Mohorovic <mohorovic@missionbio.com>
2021-03-15 15:38:38 +0100

    Add macOS support

--- build/build.sh
+++ build/build.sh
@@ -2,20 +2,7 @@ pyinstaller --additional-hooks-dir=hooks \
 			--hidden-import missionbio.mosaic \
 			--hidden-import missionbio.h5 \
 			--hidden-import plotly \
-			--clean --onefile run.py
+			--clean run.py
 
-rm -r ./build ./run.spec
-
-mkdir mosaic-streamlit-mac
-mv ./dist/run ./mosaic-streamlit-mac/run
-rm -r dist
-
-cd ..
-git archive -o latest.zip HEAD
-mv latest.zip ./build/mosaic-streamlit-mac/
-cd ./build/mosaic-streamlit-mac
-unzip latest.zip
-rm latest.zip
-cd ..
-
-# zip -r -X mosaic-streamlit-mac.zip mosaic-streamlit-mac
+cp ../*.py ./dist/run/
+cp -r ../tasks ./dist/run/
--- mosaic-streamlit/main.js
+++ mosaic-streamlit/main.js
@@ -2,17 +2,27 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const childProcess = require('child_process')
-//const fs = require('fs')
-
-//fs.writeFile('/Users/aljosa/log.txt', 'init log', (err) => {})
-const log = (msg) => {
-  //fs.appendFile('/Users/aljosa/log.txt', msg, (err) => {});
-  console.log(msg)
-}
-
-app.commandLine.appendSwitch('lang', 'en_US.UTF-8')
 
 var stMosaic
+
+const run_on_win = () => {
+  console.log('run on win')
+  const run_path = path.join(__dirname, "run", "run.exe")
+  const runtime = childProcess.spawn("powershell.exe", [run_path])
+  console.log('run on win pid', runtime.pid)
+  return runtime
+}
+
+const run_on_mac = () => {
+  console.log('run on mac')
+  const runtime = childProcess.spawn(
+    process.env.SHELL,
+    ["-c", "run"],
+    {cwd: path.join(__dirname, "run"), detached: true, env: {'LC_ALL': 'en_US.UTF-8'}}
+  )
+  console.log('run on mac pid', runtime.pid)
+  return runtime
+}
 
 function createWindow () {
   // Create the browser window.
@@ -23,63 +33,43 @@ function createWindow () {
       preload: path.join(__dirname, 'preload.js')
     }
   })
+ 
+  if(process.platform == "win32") {
+    stMosaic = run_on_win()
+  } else {
+    stMosaic = run_on_mac()
+  }
 
-  stMosaic = childProcess.spawn(
-    process.env.SHELL,
-    ['-c', './run'],
-    {cwd: path.join(__dirname, 'run'), detached: true, env: {'LC_ALL': 'en_US.UTF-8'}}
-  )
-
-  var newLine = true
   stMosaic.stdout.on('data', (data) => {
-    log(`data: ${data}`)
-    if(newLine) {
-      stMosaic.stdin.write('\n')
-      console.log('sent newline');
-      newLine = false
-    }
-  });
+    console.log(data.toString())
+    stMosaic.stdin.write('\n')
+    console.log('sent newline')
+  })
   
   stMosaic.stderr.on('data', (data) => {
-    log(`data: ${data}`)
-    console.error('error', data.toString());
-  });
-
+    console.error(data.toString())
+  })
+  
   stMosaic.on('exit', (code) => {
-    log(`data: ${data}`)
-    console.log(`Child exited with code ${code}`);
-  });
+    console.log(`Child exited with code ${code}`)
+  })
 
-  setTimeout(() => {
-    mainWindow.loadURL('http://localhost:10000/')
-  }, 3000);
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.loadURL('http://localhost:10000/')
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow()
   
   app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('will-quit', function () {
   console.log('\nwill-quit\n\n', stMosaic.pid)
-  //stMosaic.kill()
-  //process.kill(stMosaic.pid);
-  process.kill(-stMosaic.pid);
+  try {
+    process.kill(-stMosaic.pid)
+  } catch(ex) {
+    console.log('Error when trying to kill runtime', ex)
+  }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
