9795342615e0e84b653daf133d19f021fdf4e2e7
Aljosa Mohorovic <mohorovic@missionbio.com>
2021-06-18 13:11:32 +0200

    Fix precommit validations

--- src/app.py
+++ src/app.py
@@ -1,20 +1,23 @@
 # FIXME: setup Sentry for all builds until we figure out what we need
 import sys
-from sentry import setup_sentry
+
 from sentry_sdk import capture_exception
+
+from sentry import setup_sentry
 
 setup_sentry()
 
 script_runner = sys.modules["streamlit.script_runner"]
 
-streamlit_handle_exception = script_runner.handle_uncaught_app_exception
+streamlit_handle_exception = script_runner.handle_uncaught_app_exception  # type: ignore
+
 
 def catch_streamlit_exceptions(e):
     capture_exception(e)
     streamlit_handle_exception(e)
 
-script_runner.handle_uncaught_app_exception = catch_streamlit_exceptions
 
+script_runner.handle_uncaught_app_exception = catch_streamlit_exceptions  # type: ignore
 
 import streamlit as st  # noqa: E402
 
