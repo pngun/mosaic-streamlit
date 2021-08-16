1ca381e5bcfe9bf92a116184bc9b2bf1ced1eff3
Aljosa Mohorovic <mohorovic@missionbio.com>
2021-08-16 16:14:43 +0200

    Package insights code

--- build/build-macos.sh
+++ build/build-macos.sh
@@ -1,4 +1,5 @@
 pyinstaller --additional-hooks-dir=hooks \
+  --hidden-import insights \
   --hidden-import missionbio.mosaic \
   --hidden-import missionbio.h5 \
   --hidden-import analytics \
@@ -6,6 +7,4 @@ pyinstaller --additional-hooks-dir=hooks \
   --clean -w run.py
 
 rm -rf ./dist/run/
-cp -r ../src ./dist/run.app/Contents/Resources/
-cp -r ../src ./dist/run.app/Contents/MacOS/
 mv ./dist/run.app ./dist/runnable
